da3752e4421e5fd3d16b5dda1b3b46136e6f4760
Kevin <ramogikevin2024@gmail.com>
2025-02-27 23:25:32 +0300

    Initial commit of new landing page project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css"
+
+export default function App() {
+  return (
+    <main id="main-container">
+      <section id="header">
+        <nav className="header-nav">
+          <span>Logo</span>
+          <span>hamburger</span>
+        </nav>
+      </section>
+      <h1>Hello</h1>
+    </main>
+  );
+}
